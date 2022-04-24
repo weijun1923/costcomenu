8fbe107de609ff568056d01825c0a25f4106c08b
weijun1923 <asd101250101@gmail.com>
2022-04-25 00:29:36 +0800

    20220425

---
+++ js/products.js
@@ -0,0 +1,52 @@
+const products = [
+    {
+      id: 0,
+      name: "Kirkland Signature 科克蘭 鹽烤夏威夷果仁 680公克",
+      price: 669,
+      instock: 100,
+      imgSrc: "img/126979-01.jpg",
+      href: "index.html",
+    },
+    {
+      id: 1,
+      name: "J.C.'s 綜合堅果脆餅派對桶 800公克",
+      price: 339,
+      instock: 100,
+      imgSrc: "img/131133-01.jpg",
+      href: "index.html",
+    },
+    {
+      id: 2,
+      name: "萬歲牌 柿米果海苔杏仁小魚 38公克 X 20入",
+      price: 549,
+      instock: 100,
+      imgSrc: "img/132365-01.jpg",
+      href: "index.html",
+    },
+    {
+      id: 3,
+      name: "萬歲牌 綜合堅果乾優格風味 72公克 X 10入",
+      price: 469,
+      instock: 100,
+      imgSrc: "img/132368-01.jpg",
+      href: "index.html",
+    },
+    {
+      id: 4,
+      name: "Kirkland Signature 科克蘭 杏仁 1.36公斤",
+      price: 385,
+      instock: 100,
+      imgSrc: "img/995551-01.jpg",
+      href: "index.html",
+    },
+    {
+      id: 5,
+      name: "Kirkland Signature 科克蘭 核桃 1.36公斤",
+      price: 429,
+      instock: 100,
+      imgSrc: "img/999987-01.jpg",
+      href: "index.html",
+      title1: "生活用品"
+    },
+  ];
+  
