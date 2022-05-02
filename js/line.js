f0d86854d5c9346062a765da6c56761323793903
weijun1923 <asd101250101@gmail.com>
2022-05-03 02:59:17 +0800

    測試linev7

--- js/line.js
+++ js/line.js
@@ -1,8 +1,29 @@
+// <!-- 複製購物清單 -->
+
+function CopyTextToClipboard(id) {
+
+  var TextRange = document.createRange();
+
+  TextRange.selectNode(document.getElementById(id));
+
+  sel = window.getSelection();
+
+  sel.removeAllRanges();
+
+  sel.addRange(TextRange);
+
+  document.execCommand("copy");
+  // 貼上輸入框
+  const pasteText = document.querySelector('#message');
+  pasteText.focus();
+  document.execCommand('paste');
+}
+
 window.addEventListener('load', () => {
 
 var liffID = '1657102852-g5rdq53W';
 triggerLIFF();
-
+var messagetext =$("#message").val();
 
 
 function triggerLIFF() {
@@ -11,7 +32,7 @@ function triggerLIFF() {
     }).then( () => {
         const btnMessage = document.getElementById('sendMessage');
         btnMessage.addEventListener('click', () => {
-          let message = document.getElementById('message');
+          let message = messagetext;
           liff.sendMessages([
             {
               type: 'text',
