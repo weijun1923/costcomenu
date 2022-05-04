68ecda5688f2765bd8eddafc179487c8b289bcad
weijun1923 <asd101250101@gmail.com>
2022-05-04 20:33:28 +0800

    測試

--- js/line.js
+++ js/line.js
@@ -1,17 +1,15 @@
 window.addEventListener('load', () => {
-
+  
+  $('a.sendMessage').on('click', function () { 
+    let inputValue = document.getElementById("domTextElement").value;
+    document.getElementById("inputTel").innerHTML = inputValue; 
+    var $partType = $(this).closest('div.cart');
+    var $partText = $partType.find('div.setmessage').text();
+    console.log($partText);
+  });
 
 var liffID = '1657102852-g5rdq53W';
 triggerLIFF();
-
-$('a.sendMessage').on('click', function () { 
-  let inputValue = document.getElementById("domTextElement").value;
-  document.getElementById("inputTel").innerHTML = inputValue; 
-  var $partType = $(this).closest('div.cart');
-  var $partText = $partType.find('div.setmessage').text();
-  console.log($partText);
-});
-
 function triggerLIFF() {
     liff.init({
     liffId: liffID
