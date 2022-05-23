811370b0599592e9cfcf573cda879496d839cbbf
weijun1923 <asd101250101@gmail.com>
2022-05-23 16:32:55 +0800

    v4

--- js/googleSheet.js
+++ js/googleSheet.js
@@ -1,23 +1,23 @@
 let sendButton = document.getElementById('sendMessage');
 
 function send() {
-
     let phone = document.getElementById("domTextElement").value;
     const text = document.getElementById('setmessage');
     const shopingcontent = text.innerText;
     const outputContent = document.getElementById('usercontent');
     let name = outputContent.value ;
     let location = document.getElementById('reanderlocal').innerText;
+
     let total = document.getElementById('subtotal').innerText;
+    console.log(total);
     $.ajax({
-      url: "https://script.google.com/macros/s/AKfycbwTc8YEWYvjKNjyyATLVB47mVuEGY2HPhGD2BBGapR7kpZxsDWUf6izzXoJ0GhpxoDx4A/exec",
+      url: "https://script.google.com/macros/s/AKfycbxoakbL8BDw_2BbNuRuRC6KVgmsy44gr2_6isxW8mvxO_ticpgbTBEfpYthyeTUAumEbA/exec",
       data: {
           "name": name,
-          "shopingcontent": shopingcontent,
-          "phone": phone,
-          "location": location,
-          "total":total
-
+          "phone": shopingcontent,
+          "demand": phone,
+          "demand1":location,
+          "demand2":total
       },
       success: function(response) {
         if(response == "成功"){
